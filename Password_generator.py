02f9f62a60c8dd62f593ed2a980f0772939382f8
SnapDragonGR <thesanegleb@gmail.com>
2024-08-16 14:55:17 +0200

    Start of password generator project

---
+++ Password_generator.py
@@ -0,0 +1,12 @@
+import string
+import random
+
+# Getting password length
+length = int(input("Enter password length: "))
+
+print("""Choose character set for password from these:
+        1. Digits
+        2. Letters
+        3. Special characters
+        4. Exit""")
+
