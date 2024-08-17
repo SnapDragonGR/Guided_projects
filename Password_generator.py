58d7096b179388aa9253569f5571c424eafea00f
SnapDragonGR <thesanegleb@gmail.com>
2024-08-17 10:19:47 +0200

    Simple password generator guided by geeksforgeeks.org

--- Password_generator.py
+++ Password_generator.py
@@ -10,3 +10,37 @@ print("""Choose character set for password from these:
         3. Special characters
         4. Exit""")
 
+characterList = ""
+
+# Getting character set for password
+while True:
+    choice = int(input("Enter your choice: "))
+    if choice == 1:
+
+        # Adding letters to possible characters
+        characterList += string.ascii_letters
+    elif choice == 2:
+
+        # Adding digits to possible characters
+        characterList += string.digits
+    elif choice == 3:
+
+        # Adding special characters to possible characters
+        characterList += string.punctuation
+    elif choice == 4:
+        break
+    else:
+        print("Please pick a valid option")
+
+password = []
+
+for _ in range(length):
+
+    # Picking a random character from the character list
+    randomchar = random.choice(characterList)
+
+    # Appending a random character to password
+    password.append(randomchar)
+
+# Printing password as a string
+print("The random password is " + "".join(password))
